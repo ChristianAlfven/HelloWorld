c1f3a338e24fc18ef1c11f9f0228bcab77d9b8d3
marku <marku@DESKTOP-TCIT586>
2019-04-08 13:02:20 +0200

    Not commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Not Hello World!");
+        System.out.println("Not Not Hello World!");
         System.out.println("Hello Markus");
         System.out.println("Hello C");
     }
