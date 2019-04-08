6ffaf57fb94e9003bda1d7c7334dbc2de88f141d
pellejnsn <hejper@live.se>
2019-04-08 12:53:37 +0200

    Hej jag ändrade allt

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("Hello Markus");
         System.out.println("Hello C");
+        System.out.println("Hello Pelle");
     }
+
 }
