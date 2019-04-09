9dacda3d91cd9ade267e20c5a948dbb330beedb7
pellejnsn <hejper@live.se>
2019-04-09 13:11:21 +0200

    dsa

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         System.out.println("Pull Request new");
         System.out.println("Pelle");
 
+        System.out.println("Pelle");
+        System.out.println("Pelle");
 
     }
 }
